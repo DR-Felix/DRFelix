efbb17ab992dc5b13a7a3d6794993c5c4598b32e
DRFelix <56882425+DR-Felix@users.noreply.github.com>
2022-10-11 01:12:34 +0300

    Add files via upload

---
+++ shestakov_n_a/1717b.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int main()
+{
+	int t;
+	int n, k, r, c;
+	std::cin >> t;
+	for (int a = 0; a < t; a += 1) {
+		std::cin >> n >> k >> r >> c;
+		for (int i = 1; i <= n; i += 1) {
+			for (int j = 1; j <= n; j += 1) {
+				if ((i + j) % k == (r + c) % k) {
+					std::cout << "X";
+				}
+				else {
+					std::cout << ".";
+				}
+			}
+			std::cout << "\n";
+		}
+	}
+	return 0;
+}
