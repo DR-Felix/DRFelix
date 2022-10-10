856c61fbd6481c024cec0e831f6e97f68eaeffd4
DRFelix <56882425+DR-Felix@users.noreply.github.com>
2022-10-11 01:11:56 +0300

    Add files via upload

---
+++ shestakov_n_a/1721b.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <array>
+
+int main() {
+    int t;
+    std::cin >> t;
+    for (int i = 0; i < t; i += 1) {
+        int n, m, s_x, s_y, d;
+        std::cin >> n >> m >> s_x >> s_y >> d;
+        if (s_y + d >= m && s_y - d <= 1) {
+            std::cout << -1 << '\n'; // если лазер полностью покрывает столбец
+        }
+        else if (s_x + d >= n && s_x - d <= 1) {
+            std::cout << -1 << '\n'; // если лазер полностью покрывает строку
+        }
+        else if (s_y - d <= 1 && s_x - d <= 1) {
+            std::cout << -1 << '\n'; // если лазер перекрывает стартовую позицию диагональю
+        }
+        else if (s_y + d >= m && s_x + d >= n) {
+            std::cout << -1 << '\n'; // если лазер перекрывает конечную позицию диагональю
+        }
+        else {
+            std::cout << n + m - 2 << '\n';
+        }
+    }
+    return 0; 
+}
