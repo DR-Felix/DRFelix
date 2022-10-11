27f3222f9633fadeefefc4b143d5ad7eddc3c361
DRFelix <56882425+DR-Felix@users.noreply.github.com>
2022-10-11 14:50:11 +0300

    Add files via upload

---
+++ shestakov_n_a/0915a.cpp
@@ -0,0 +1,18 @@
+﻿#define _USE_MATH_DEFINES
+#include <cmath>
+#include <iostream>
+
+int main() {
+    int n, k, a, t;
+    t = 10000000000;
+    std::cin >> n >> k;
+    for (int i = 0; i < n; i += 1) {
+        std::cin >> a;
+        if (k % a == 0) {
+            if (k / a < t) {
+                t = k / a;
+            }
+        }
+    }
+    std::cout << t;
+}
