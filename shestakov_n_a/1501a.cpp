cda6a27ca36856d53180ba4ed51fea763d6f5c77
DRFelix <56882425+DR-Felix@users.noreply.github.com>
2022-11-01 13:49:08 +0300

    Add files via upload

---
+++ shestakov_n_a/1501a.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+
+int main() {
+	int t;
+	std::cin >> t;
+	for (int k = 0; k < t; k += 1) {
+		int n;
+		std::cin >> n;
+		int ai = 0;
+		int bi = 0;
+		int time = 0;
+		std::vector <int> a(101);
+		std::vector <int> b(101);
+		std::vector <int> t(101);
+		for (int i = 1; i <= n; i += 1) {
+			std::cin >> a[i] >> b[i];
+		}
+		for (int i = 1; i <= n; i += 1) {
+			std::cin >> t[i];
+		}
+		for (int i = 0; i < n; i += 1) {
+			if (b[i] > time + (b[i] - a[i] + 1) / 2) {
+				time = b[i];
+			}
+			else {
+				time = time + (b[i] - a[i] + 1) / 2;
+			}
+			time += a[i + 1] - b[i] + t[i + 1];
+		}
+		std::cout << time << std::endl;
+	}
+
+}
