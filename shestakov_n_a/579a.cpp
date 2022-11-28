18266511b09a3ca4d4d3f22f2b89b589c794bbde
DRFelix <56882425+DR-Felix@users.noreply.github.com>
2022-11-29 01:36:34 +0300

    Add files via upload

---
+++ shestakov_n_a/579a.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+int main() {
+    int x = 0;
+    std::cin >> x;
+    int k = 0;
+    while (x > 0) {
+        if (x % 2 == 1) {
+            k += 1;
+        }
+        x = x / 2;
+    }
+    std::cout << k;
+    return 0;
+}
