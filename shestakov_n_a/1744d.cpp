2b0898678b92773984977980b1ae10860ff9a817
DRFelix <56882425+DR-Felix@users.noreply.github.com>
2022-11-28 11:47:36 +0300

    Add files via upload

---
+++ shestakov_n_a/1744d.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+
+int main() {
+    int t = 0;
+    std::cin >> t;
+    for (int i = 0; i < t; i += 1) {
+        int n = 0;
+        std::cin >> n;
+        int k = 0;
+        for (int j = 0; j < n; j += 1) {
+            int a = 0;
+            std::cin >> a;
+            while (a % 2 == 0) {
+                k += 1;
+                a /= 2;
+            }
+        }
+        if (k >= n) {
+            std::cout << 0 << std::endl;
+        }
+        else {
+            int ans = 0;
+            int lg = int(std::log2(n));
+            while (k != n && lg > 0) {
+                if (k + lg <= n) {
+                    k += lg;
+                    ans += 1;
+                }
+                lg = lg - 1;
+            }
+            if (k != n) {
+                std::cout << -1 << std::endl;
+            }
+            else {
+                std::cout << ans << std::endl;
+            }
+        }
+    }
+}
