712fd006c79fa599191c347a8722f767e00129dc
DRFelix <56882425+DR-Felix@users.noreply.github.com>
2022-11-29 13:02:46 +0300

    Add files via upload

---
+++ shestakov_n_a/1699b.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+
+int main() {
+    int t = 0;
+    std::cin >> t;
+    for (int i = 0; i < t; i += 1) {
+        int n = 0;
+        int m = 0;
+        std::cin >> n >> m;
+        for (int j = 1; j <= n; j += 1) {
+            for (int k = 1; k <= m; k += 1) {
+                std::cout << ((j % 4 <= 1) != (j % 4 <= 1)) << ' ';
+            }
+            std::cout << std::endl;
+        }
+    }
+    return 0;
+}
