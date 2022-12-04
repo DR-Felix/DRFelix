d10ce44a6b14ccf1d2f78cc09781186a16a39209
DRFelix <56882425+DR-Felix@users.noreply.github.com>
2022-12-04 17:22:34 +0300

    Add files via upload

---
+++ shestakov_n_a/1165b.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+#include <algorithm> // для функции sort()
+#include <vector>
+
+int main() {
+    int n = 0;
+    std::cin >> n;
+    std::vector <int> arr;
+    arr.resize(n);
+    for (int i = 0; i < n; i += 1) {
+        int a = 0;
+        std::cin >> a;
+    }
+    std::sort(arr.begin(), arr.end());
+    int day = 1;
+    int count = 0;
+    for (int i = 0; i < n; i += 1) {
+        if (arr[i] >= day) {
+            count += 1;
+            day += 1;
+        }
+    }
+    std::cout << count;
+    return 0;
+}
