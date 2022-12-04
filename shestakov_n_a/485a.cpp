97aefc842da2aeca89602beb09f0e6e8a34e4606
DRFelix <56882425+DR-Felix@users.noreply.github.com>
2022-12-04 17:48:18 +0300

    Add files via upload

---
+++ shestakov_n_a/485a.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <set>
+
+int main() {
+    std::set <int> det;
+    int a = 0;
+    int m = 0;
+    std::cin >> a >> m;
+    bool fl = false;
+    while (!det.contains(a % m)) {
+        det.insert(a % m);
+        a = (a % m) * 2;
+        if (a == 0) {
+            fl = true;
+            break;
+        }
+    }
+    if (fl) {
+        std::cout << "Yes";
+    }
+    else {
+        std::cout << "No";
+    }
+}
