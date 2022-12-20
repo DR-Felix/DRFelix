f9b5f436f5f4dc6a4ae52bb1d5b6c618e0e82cd2
DRFelix <56882425+DR-Felix@users.noreply.github.com>
2022-12-20 15:58:10 +0300

    Add files via upload

---
+++ shestakov_n_a/bpm_22_1_2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main() {
+    double n = 0;
+    double k = 0;
+    double x = 0;
+    double y = 0;
+    while (std::cin >> x >> y) {
+        if ((x * x + y * y <= 4) && (y >= 2 - x * x)) {
+            k += 1;
+        }
+        n += 1;
+    }
+    std::cout << k / n;
+}
